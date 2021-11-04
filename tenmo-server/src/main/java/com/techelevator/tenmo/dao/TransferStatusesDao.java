7874ee8bc2c72c622de9a8ac378903c68e98829a
Matthew Kirby <matt.kirby2011@gmail.com>
2021-11-04 18:06:53 -0400

    Update before Pulling Main

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferStatusesDao.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.tenmo.dao;
-
-import com.techelevator.tenmo.model.TransferStatuses;
-
-public interface TransferStatusesDao {
-
-    TransferStatuses getTransferStatus(long transferTypeID);
-
-}
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferTypesDao.java
+++
@@ -1,8 +0,0 @@
-package com.techelevator.tenmo.dao;
-
-import com.techelevator.tenmo.model.TransferTypes;
-
-public interface TransferTypesDao {
-
-    TransferTypes getTransferTypes(long typeID);
-}
