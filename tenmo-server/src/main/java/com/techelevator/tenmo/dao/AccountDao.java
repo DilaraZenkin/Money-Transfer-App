2c19c0e6c383e575e1ba13f692168a07b9e209d4
dilarazenkin <dilaraznkn@gmail.com>
2021-11-04 17:02:30 -0400

    accountDao

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,20 +1,14 @@
 package com.techelevator.tenmo.dao;
 
-<<<<<<< HEAD
-public interface AccountDao {
-
-
-=======
 import com.techelevator.tenmo.model.Account;
 
 import java.math.BigDecimal;
 
 public interface AccountDao {
 
-    Account getBalance(long accountID);
 
-    Account increaseBalance(long accountID);
-
-    Account decreaseBalance(long accountID);
+    Account getAccountById (long accountID);
+    BigDecimal getBalance(long accountID);
+    BigDecimal increaseBalance(BigDecimal addMoney, long accountID);
+    BigDecimal decreaseBalance(BigDecimal subtractMoney,long accountID);
 }
->>>>>>> 4a9af62f6a76853d626e05d56bd27705d4fea1d8
