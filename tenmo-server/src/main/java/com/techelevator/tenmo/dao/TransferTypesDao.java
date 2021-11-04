afdd2c5db1738e2afd11a31f8881b56bb85bcdef
Matthew Kirby <matt.kirby2011@gmail.com>
2021-11-04 10:49:14 -0400

    Update for Start of Day

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferTypesDao.java
@@ -0,0 +1,8 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.TransferTypes;
+
+public interface TransferTypesDao {
+
+    TransferTypes getTransferTypes(long typeID);
+}
